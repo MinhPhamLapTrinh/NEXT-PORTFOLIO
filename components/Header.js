61769f4e905fa2f9930ed4eb2832620059641cfc
MinhPhamLapTrinh <116688660+MinhPhamLapTrinh@users.noreply.github.com>
2024-04-21 12:39:30 -0400

    initial commit

---
+++ components/Header.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+const links = [
+  {
+    name: "About me",
+    href: "/about",
+  },
+  {
+    name: "Projects",
+    href: "/projects",
+  },
+  {
+    name: "Skills",
+    href: "/skills",
+  },
+  {
+    name: "Contact",
+    href: "/contact",
+  },
+];
+export default function Header() {
+    return(
+        <>
+            
+        </>
+    )
+}
