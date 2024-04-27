d27204e0d129cf9493aa1992e2e7e070ba6034a5
MinhPhamLapTrinh <116688660+MinhPhamLapTrinh@users.noreply.github.com>
2024-04-26 22:21:09 -0400

    update project component

--- components/Project.js
+++ components/Project.js
@@ -1,13 +1,14 @@
 import Image from "next/image";
-import dev1 from "../public/dev.png";
-import dev2 from "../public/toji.png";
+import personalWeb1 from "../public/personal-web/personal-web-1.png";
+import personalWeb2 from "../public/personal-web/personal-web-2.png";
+import personalWeb3 from "../public/personal-web/personal-web-3.png";
 import { MdNavigateNext } from "react-icons/md";
 import { GrFormPrevious } from "react-icons/gr";
 import { useState } from "react";
-import Link from "next/link";
+
 export default function Project({ description, imageList, gitHubLink }) {
   const [nextImg, setNextImg] = useState(0);
-  const img = [dev1, dev2];
+  const img = [personalWeb1, personalWeb2, personalWeb3];
 
   const handleNextImage = () => {
     setNextImg((nextImg + 1) % img.length);
@@ -23,36 +24,41 @@ export default function Project({ description, imageList, gitHubLink }) {
     <>
       <div className="flex flex-col space-y-10 items-center justify-center pb-5">
         <h1 className="text-5xl font-sm">My Works</h1>
-        <div className="flex flex-col space-y-10 px-2 w-3/5">
-          <div className="flex justify-between space-x-20 bg-transparent rounded-lg">
-            <span className="px-12 py-5">This is my personal website</span>
+        <div className="flex flex-col space-y-10 px-2 w-3/4">
+          <div className="flex flex-row justify-around bg-[#F0EBE3] rounded-lg">
+            <span className="flex px-10 py-5 w-1/2">
+              This is my personal website. This is my personal website. This is
+              my personal website. This is my personal website. This is my
+              personal website. This is my personal website. This is my personal
+              website. This is my personal website.
+            </span>
             <div className="relative">
               <Image
                 src={img[(nextImg + 1) % img.length]}
-                width="390"
-                height="200"
+                width="700"
+                height="400"
                 alt="pics"
                 className="shadow-2xl rounded-t-2xl rounded-br-lg mt-10 absolute bottom-0"
               />
               <Image
                 src={img[nextImg]}
-                width="400"
-                height="200"
+                width="700"
+                height="400"
                 alt="pics"
                 className="shadow-2xl rounded-t-2xl rounded-br-lg mt-10"
               />
-              <div className="absolute bottom-20 pb-10 left-0 flex items-center">
+              <div className="absolute  bottom-20 pb-10 left-0 flex items-center">
                 <button
-                  className="bg-slate-300 bg-opacity-80 rounded-full px-2 py-2"
+                  className="bg-slate-300 bg-opacity-60 rounded-full px-2 py-2"
                   type="button"
                   onClick={handlePreviousImage}
                 >
                   <GrFormPrevious />
                 </button>
               </div>
-              <div className="absolute bottom-20 pb-10 right-0 flex items-center">
+              <div className="absolute ml-10 bottom-20 pb-10 right-0 flex items-center">
                 <button
-                  className="bg-slate-300 bg-opacity-80 rounded-full px-2 py-2"
+                  className="bg-slate-300 bg-opacity-60 rounded-full px-2 py-2"
                   type="button"
                   onClick={handleNextImage}
                 >
@@ -61,7 +67,7 @@ export default function Project({ description, imageList, gitHubLink }) {
               </div>
             </div>
           </div>
-          <div className="flex justify-between space-x-20 bg-gray-50 px-2 py-2 rounded-lg">
+          {/* <div className="flex justify-between space-x-20 bg-gray-50 px-2 py-2 rounded-lg">
             <span>This is my personal website</span>
             <div className="relative">
               <Image
@@ -90,7 +96,7 @@ export default function Project({ description, imageList, gitHubLink }) {
                 </button>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </>
